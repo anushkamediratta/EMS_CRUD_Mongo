48c9392435438c6f9079345462868fd534b57b51
ANUSHKA <anushkamediratta>
2023-06-14 00:01:23 +0530

    check

--- controllers/routes.js
+++ controllers/routes.js
@@ -5,6 +5,7 @@ const router=express.Router()
 
 router.get("/",(req,res)=>{
     res.render("home")
+    console.log("hello");
 })
 
 router.get("/add-emp",(req,res)=>{
@@ -25,12 +26,9 @@ router.post("/save-emp", async (req, res) => {
       console.log("Save Error: ", err);
     }
   });
-  
-
-
+ 
 router.get("/show-all-emp",async (req,res)=>{
-// 
-
+git 
 try {
     const result = await Employee.find().exec();
     res.render('showEmp', { list: result });
@@ -39,6 +37,63 @@ try {
   }
   
 })
+
+router.get("/delete-all-emp", async (req, res) => {
+  try {
+    const result = await Employee.find();
+    res.render('delete', { list: result });
+  } catch (err) { 
+    console.error(err);
+  }
+});
+
+router.get("/final-delete/:uid", async (req, res) => {
+  try {
+    const result = await Employee.findByIdAndDelete(req.params.uid);
+    res.redirect('/emp/delete-all-emp');
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.get("/update-all-emp", async (req, res) => {
+  try {
+    
+    const result = await Employee.find();
+    res.render('updateEmp', { list: result });
+  } catch (err) { 
+    console.error(err);
+  }
+});
+
+router.get("/pre-update/:id", async (req, res) => {
+  try {
+    const result = await Employee.findById(req.params.id);
+    res.render('preEmp', { emp: result });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// router.post("/final-update",(req,res)=>{
+//   console.log(req.body)
+//   Employee.findByIdAndUpdate(req.body.id,req.body,{new:true},(err,result)=>{
+//     if(err){
+//       console.log(err)
+//     }else{
+//       res.redirect("/emp/update-all-emp")
+//     }
+//   })
+// })
+
+router.post("/final-update", async (req, res) => {
+  try {     
+    await Employee.findByIdAndUpdate(req.body.id, req.body, { new: true });
+    res.redirect('/emp/update-all-emp');
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 
 module.exports=router
